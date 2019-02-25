54dade4aa94fe334e8a69d77fb1ac52d71083b31
Guillaume Lafranceschina <contact@guillaume.pro>
2019-02-25 11:56:01 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nexrender-action-handbrake
---
+++ index.js
@@ -0,0 +1,33 @@
+const {name}  = require('./package.json')
+const hbjs = require('handbrake-js');
+
+module.exports = (job, settings, options, type) => {
+    if (type != 'postrender') {
+        throw new Error(`Action ${name} can be only run in postrender mode, you provided: ${type}.`)
+    }
+
+    settings.logger.log(`[${job.uid}] starting action-handbrake action`)
+
+    return new Promise((resolve, reject) => {
+        // Handbrake
+        let input = options.input || job.output;
+
+        hbjs.spawn({ input: input, output: options.output })
+            .on('error', err => {
+                // invalid user input, no video found etc
+                console.log(err);
+                return reject(new Error('Error in action-handbrake module'));
+            })
+            .on('progress', progress => {
+                console.log(
+                    'Percent complete: %s, ETA: %s',
+                    progress.percentComplete,
+                    progress.eta
+                )
+            })
+            .on('end', () => {
+                resolve(job);
+            })
+        });
+    });
+}
