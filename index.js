b583f7fd04bb7f8936cc622b5fcde6cd21b13100
Guillaume Lafranceschina <contact@guillaume.pro>
2019-02-25 14:47:39 +0100

    Rework of the output path, debug option

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {name}  = require('./package.json')
 const hbjs = require('handbrake-js');
+const path    = require('path')
 
 module.exports = (job, settings, options, type) => {
     if (type != 'postrender') {
@@ -11,19 +12,38 @@ module.exports = (job, settings, options, type) => {
     return new Promise((resolve, reject) => {
         // Handbrake
         let input = options.input || job.output;
+        let output, filename;
+
+        if(options.output){
+            output = options.output;
+        }
+        else{
+            // if there is not output specified, we use outputFilename
+            // (or 'encoded' by default) and outputExt as the extension
+            // (or mp4 by default)
+            filename = `${options.outputFilename || "encoded"}.${options.outputExt || "mp4"}`;
+
+            output = path.join(job.workpath, filename);
+        }
+
+        if(options.debug){
+            settings.logger.log(`[${job.uid}] [action-handbrake] output is set to ${output}`)
+        }
 
         hbjs.spawn({ input: input, output: options.output })
             .on('error', err => {
                 // invalid user input, no video found etc
-                console.log(err);
+                if(options.debug){
+                    console.log(err);
+                }
                 return reject(new Error('Error in action-handbrake module'));
             })
             .on('progress', progress => {
-                console.log(
-                    'Percent complete: %s, ETA: %s',
-                    progress.percentComplete,
-                    progress.eta
-                )
+                if(options.debug){
+                    console.log(
+                        `[${job.uid}] [action-handbrake] percent complete : ${progress.percentComplete}, ETA: ${progress.eta}`
+                    )
+                }
             })
             .on('end', () => {
                 resolve(job);
